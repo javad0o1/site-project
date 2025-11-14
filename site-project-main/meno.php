f43aa48042ab48d80800f9cfea6a3073284d1db6
javad0o1 <javad.shariati990@gmail.com>
2025-11-14 16:07:17 +0330

    Add files via upload

---
+++ site-project-main/meno.php
@@ -0,0 +1,16 @@
+ <header class="header">
+        <div class="logo">Childrens</div>
+        <nav class="nav-links">
+            <a href="site.php" id="al1" >صفحه اصلی</a>
+            <a href="#">تازه‌ها</a>
+            <a href="#">دخترانه</a>
+            <a href="#">پسرانه</a>
+            <a href="#">تخفیف‌ها</a>
+            <a href="درباره ما.php">درباره ما</a>
+        </nav>
+        <div class="icons">
+            <span class="icon">🔍</span>
+            <span class="icon">🛒</span>
+            <a href="login page.php" id="aicons" >👤</a>
+        </div>
+    </header>
